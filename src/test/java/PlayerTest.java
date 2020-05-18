18770e3c3f1b175b234e570e39bd1401af46cf70
rkaszubski <59755278+rkaszubski@users.noreply.github.com>
2020-05-18 16:14:03 -0500

    more tests

--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -4,24 +4,46 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerTest {
 
+    Player p = new Player("Bob",1000);
+
     @Test
     void getName() {
+        assertEquals("Bob",p.getName());
     }
 
     @Test
     void getHand() {
+        Hand expect = new Hand();
+        assertEquals(expect.toString(),p.getHand().toString());
+        expect.add(new Card(Face.ACE,Suit.DIAMONDS));
+        p.getHand().add(new Card(Face.ACE,Suit.DIAMONDS));
+        assertEquals(expect.toString(),p.getHand().toString());
     }
 
     @Test
     void clearHand() {
+        Hand expect = new Hand();
+        p.getHand().add(new Card(Face.ACE,Suit.DIAMONDS));
+        p.clearHand();
+        assertEquals(expect.toString(),p.getHand().toString());
+
     }
 
     @Test
     void getCash() {
+        assertEquals(p.getCash(),1000);
     }
 
     @Test
-    void adjustCash() {
+    void adjustCashPos() {
+        p.adjustCash(50);
+        assertEquals(1050,p.getCash());
+    }
+
+    @Test
+    void adjustCashNeg() {
+        p.adjustCash(-50);
+        assertEquals(950,p.getCash());
     }
 
     @Test
