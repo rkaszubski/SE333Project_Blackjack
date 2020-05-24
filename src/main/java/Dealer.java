bb949311c1d407e9f8059a6081e44a421ff039b0
rkaszubski <59755278+rkaszubski@users.noreply.github.com>
2020-05-24 18:42:44 -0500

    new tests

--- src/main/java/Dealer.java
+++ src/main/java/Dealer.java
@@ -13,26 +13,60 @@ public class Dealer{
         this.players = new ArrayList<Player>();
         this.hand = new Hand();
     }
+
+    public Boolean verifyPlayerCount(String input){
+        int in;
+        try{
+            in = Integer.valueOf(input);
+            if(in <= 0){
+                System.out.println("Your input must be greater than 0");
+                return false;
+            }
+            else if(in > 5){
+                System.out.println("Your input must be 5 or less");
+                return false;
+            }
+
+            return true;
+
+            }
+        catch (Exception e){
+            System.out.println("Your input must be an integer value");
+            return false;
+        }
+    }
+
+    public void addPlayer(String name){
+        players.add(new Player(name, 1000));
+    }
+
     public void startGame(){
         Scanner scanner = new Scanner(System.in);
         String name;
-        int input;
+        String in;
 
         System.out.println("How many players?");
-        input = Integer.valueOf(scanner.nextLine());
+        in = (scanner.nextLine());
+        while (!verifyPlayerCount(in)){
+            in = (scanner.nextLine());
+        }
+        int input = Integer.valueOf(in);
         int playernum = 1;
         while (input > 0){
             System.out.println("What is Player #" + playernum +"'s name?");
             name = scanner.nextLine();
-            players.add(new Player(name,1000));
+            addPlayer(name);
             playernum++;
             input--;
         }
     }
 
-    public void dealCards(ArrayList<Player> players){
+    public void newDeck(){
         deck = new Deck();
         deck.shuffleDeck();
+    }
+
+    public void dealCards(ArrayList<Player> players){
 
         for(Player player : players){
             player.getHand().add(deck.drawCard());
@@ -57,6 +91,7 @@ public class Dealer{
         }
         System.out.println("All bets are locked in");
         System.out.println("Dealing Cards!");
+        newDeck();
         dealCards(players);
 
         //Hit or Stand
@@ -91,26 +126,17 @@ public class Dealer{
 
         //Reveal winners
         for(Player player : players){
-            int playersum = player.getHand().sum();
-            if(playersum > 21){
-                System.out.println(player.getName() + " has busted!");
+            int result = winorlose(player.getHand(),hand);
+            if (result == 0){
+                System.out.println(player.getName() + " lost!");
                 System.out.println(player.getName() + "'s new balance is " + player.getCash());
             }
-            else if(hand.sum()> 21){
-                System.out.println(player.getName() + " won! Dealer Busted!");
-                player.adjustCash(player.getBet()*2);
-                System.out.println(player.getName() + "'s new balance is " + player.getCash());
-            }
-            else if(playersum > hand.sum()){
+            else if(result == 1){
                 System.out.println(player.getName() + " won!");
                 player.adjustCash(player.getBet()*2);
                 System.out.println(player.getName() + "'s new balance is " + player.getCash());
             }
-            else if(playersum < hand.sum()){
-                System.out.println(player.getName() + " lost!");
-                System.out.println(player.getName() + "'s new balance is " + player.getCash());
-            }
-            else if(playersum == hand.sum()){
+            else if(result == 2){
                 System.out.println(player.getName() + " tied!");
                 player.adjustCash(player.getBet());
                 System.out.println(player.getName() + "'s new balance is " + player.getCash());
@@ -123,6 +149,31 @@ public class Dealer{
 
         }
         hand = new Hand();
+
+    }
+
+    public int winorlose(Hand playerhand, Hand dealerhand){
+        //if player wins returns 1
+        //if player loses returns 0
+        //if player ties returns 2
+        int playersum = playerhand.sum();
+        int dealersum = dealerhand.sum();
+        if(playersum > 21){
+            return 0;
+        }
+        else if(dealersum> 21){
+            return 1;
+        }
+        else if(playersum > dealersum){
+            return 1;
+        }
+        else if(playersum < dealersum){
+            return 0;
+        }
+        else if(playersum == dealersum){
+            return 2;
+        }
+        return 0;
 
     }
 
--- src/test/java/DealerTest.java
+++ src/test/java/DealerTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -6,15 +7,91 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class DealerTest {
 
-    Dealer dealer = new Dealer();
-
-    Player p1 = new Player("Bob",1000);
-    Player p2 = new Player("Rob",1000);
-    Player p3 = new Player("Sob",1000);
+    Dealer d1 = new Dealer();
 
 
     @Test
     void dealCards() {
 
+    }
+    @Test
+    void verifyPlayerCount(){
+        assertTrue(d1.verifyPlayerCount("3"));
+        assertFalse(d1.verifyPlayerCount(null));
+        assertFalse(d1.verifyPlayerCount(" "));
+        assertFalse(d1.verifyPlayerCount("0"));
+        assertFalse(d1.verifyPlayerCount("6"));
+    }
+
+    @Test
+    @DisplayName("Win - Dealer bust")
+    void winorlose_winbust(){
+        Hand dh = new Hand();
+        dh.add(new Card(Face.KING,Suit.DIAMONDS));
+        dh.add(new Card(Face.KING,Suit.CLUBS));
+        dh.add(new Card(Face.KING,Suit.HEARTS));
+        Hand ph = new Hand();
+        ph.add(new Card(Face.QUEEN,Suit.DIAMONDS));
+        ph.add(new Card(Face.QUEEN,Suit.CLUBS));
+        assertEquals(1, d1.winorlose(ph,dh));
+    }
+    @Test
+    @DisplayName("Win - Player > Dealer - No Busts")
+    void winorlose_winnorm(){
+        Hand dh = new Hand();
+        dh.add(new Card(Face.KING,Suit.DIAMONDS));
+        dh.add(new Card(Face.SEVEN,Suit.CLUBS));
+        Hand ph = new Hand();
+        ph.add(new Card(Face.QUEEN,Suit.DIAMONDS));
+        ph.add(new Card(Face.QUEEN,Suit.CLUBS));
+        assertEquals(1, d1.winorlose(ph,dh));
+    }
+    @Test
+    @DisplayName("Lose - Player Bust")
+    void winorlose_losebust(){
+        Hand dh = new Hand();
+        dh.add(new Card(Face.KING,Suit.DIAMONDS));
+        dh.add(new Card(Face.SEVEN,Suit.CLUBS));
+        Hand ph = new Hand();
+        ph.add(new Card(Face.QUEEN,Suit.DIAMONDS));
+        ph.add(new Card(Face.QUEEN,Suit.CLUBS));
+        ph.add(new Card(Face.QUEEN,Suit.HEARTS));
+        assertEquals(0, d1.winorlose(ph,dh));
+    }
+    @Test
+    @DisplayName("Lose - Player and Dealer Bust")
+    void winorlose_losebothbust(){
+        Hand dh = new Hand();
+        dh.add(new Card(Face.KING,Suit.DIAMONDS));
+        dh.add(new Card(Face.KING,Suit.CLUBS));
+        dh.add(new Card(Face.KING,Suit.HEARTS));
+        Hand ph = new Hand();
+        ph.add(new Card(Face.QUEEN,Suit.DIAMONDS));
+        ph.add(new Card(Face.QUEEN,Suit.CLUBS));
+        ph.add(new Card(Face.QUEEN,Suit.HEARTS));
+        assertEquals(0, d1.winorlose(ph,dh));
+    }
+    @Test
+    @DisplayName("Lose - Dealer > Player")
+    void winorlose_losenorm(){
+        Hand dh = new Hand();
+        dh.add(new Card(Face.KING,Suit.DIAMONDS));
+        dh.add(new Card(Face.KING,Suit.CLUBS));
+        Hand ph = new Hand();
+        ph.add(new Card(Face.QUEEN,Suit.DIAMONDS));
+        ph.add(new Card(Face.SEVEN,Suit.CLUBS));
+        assertEquals(0, d1.winorlose(ph,dh));
+    }
+
+    @Test
+    @DisplayName("Tie")
+    void winorlose_tie(){
+        Hand dh = new Hand();
+        dh.add(new Card(Face.KING,Suit.DIAMONDS));
+        dh.add(new Card(Face.KING,Suit.CLUBS));
+        Hand ph = new Hand();
+        ph.add(new Card(Face.QUEEN,Suit.DIAMONDS));
+        ph.add(new Card(Face.QUEEN,Suit.CLUBS));
+        assertEquals(2, d1.winorlose(ph,dh));
     }
 }
